5e79c6f8cae18ee578365a11f9de4802b751f539
Alvaro Saburido <alvaro.saburido@gmail.com>
2020-08-01 19:10:53 +0200

    feat(clipboard): clipbboard button and animations

--- pages/result/index.vue
+++ pages/result/index.vue
@@ -1,9 +1,16 @@
 <template>
   <div class="result page">
     <div class="container">
-      <div class="grid grid-cols-1 sm:grid-cols-2">
-        <div class="card result-card mb-8 mx-auto">
-          <div class="p-6 text-gray-700 text-base">
+      <div class="grid grid-cols-1 md:grid-cols-2">
+        <div
+          ref="result-card"
+          class="card result-card mb-8 mx-auto relative scroll-gradient"
+        >
+          <div
+            class="p-6 text-gray-700 text-base"
+            :class="scrollStyles"
+            @scroll="onResultScroll"
+          >
             <Ipsum v-if="query" v-bind="query" />
           </div>
         </div>
@@ -17,12 +24,37 @@
 </template>
 
 <script>
+const data = () => ({
+  gradientBottom: true,
+  gradientTop: false,
+});
+
+const computed = {
+  scrollStyles() {
+    return {
+      'scroll-gradient': true,
+      'scroll-gradient--top': this.gradientTop,
+      'scroll-gradient--bottom': this.gradientBottom,
+    };
+  },
+};
 const watch = {
   $route({ query }) {
     this.query = query;
   },
 };
+const methods = {
+  onResultScroll({ target: { scrollTop, clientHeight, scrollHeight } }) {
+    console.log({ scrollTop, clientHeight, scrollHeight });
+
+    this.gradientBottom = scrollTop + clientHeight < scrollHeight;
+    this.gradientTop = scrollTop > 0;
+  },
+};
 export default {
+  data,
+  computed,
+  methods,
   asyncData({ query }) {
     const { paragraphs, spicy, startWithArepa } = query;
     return {
@@ -41,8 +73,41 @@ export default {
 .card {
   @apply w-full;
 }
-.result-card {
+.clipboard-btn {
+  right: -8px;
+  top: -8px;
+  z-index: 1;
+}
+
+.scroll-gradient::before,
+.scroll-gradient::after {
+  content: '';
+  position: absolute;
+  height: 100px;
+  width: 100%;
+  left: 0;
+}
+
+.scroll-gradient--bottom::before {
+  bottom: 0;
+  background: linear-gradient(
+    0deg,
+    rgba(255, 255, 255, 1) 0%,
+    rgba(0, 212, 255, 0) 100%
+  );
+}
+
+.scroll-gradient--top::after {
+  top: 0;
+  background: linear-gradient(
+    180deg,
+    rgba(255, 255, 255, 1) 0%,
+    rgba(0, 212, 255, 0) 100%
+  );
+}
+
+.result-card > div {
   overflow: scroll;
-  max-height: 600px;
+  max-height: 500px;
 }
 </style>
