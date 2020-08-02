5c3f1892f91df2de00554ddd0aaf20e1193e4e9e
Alvaro Saburido <alvaro.saburido@gmail.com>
2020-08-02 18:06:32 +0200

    feature(footer): added cool footer

---
+++ components/footer/Footer.vue
@@ -0,0 +1,74 @@
+<template>
+  <footer class="footer w-full bg-pernil-900 p-6 relative">
+    <div class="container h-full flex flex-wrap items-center">
+      <div class="w-full md:w-1/3 order-2 md:order-1 text-center md:text-left">
+        <h2 class="text-2xl text-pernil-200 mb-4 ">
+          {{ $t('venezuela-true-love') }} 🥑
+        </h2>
+        <span class="copyright text-xs text-pernil-400"
+          >© Copyright
+          <a
+            class="text-aguacate-500 hover:text-aguacate-600"
+            href="https://alvarosaburido.com/"
+            rel="noopener"
+            target="_blank"
+            >Alvaro Saburido</a
+          >
+          2020.</span
+        >
+      </div>
+      <div class="w-full md:w-1/3 order-1 mb-4 md:order-2">
+        <img
+          class="footer-logo mx-auto mb-6"
+          src="@/assets/img/icon.svg"
+          alt="Footer Logo"
+        />
+        <LanguageMenu class="footer-language-menu" />
+      </div>
+      <div class="w-full md:w-1/3 mb-4 order-3"></div>
+      <div
+        class="absolute w-full hidden md:block py-2 bottom-0 left-0 bg-black text-gray-800 text-xs"
+      >
+        <div class="container">
+          {{ $t('proudly-made-with') }}
+          <a
+            class="text-gray-700 hover:text-gray-600"
+            href="https://nuxtjs.org/"
+            rel="noopener"
+            target="_blank"
+            >Nuxt</a
+          >, hosting
+
+          <a
+            class="text-gray-700 hover:text-gray-600"
+            href="https://www.netlify.com//"
+            rel="noopener"
+            target="_blank"
+            >Netlify</a
+          >.
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.footer {
+  @media (min-width: 640px) {
+    height: 250px;
+  }
+}
+
+.footer-logo {
+  width: 64px;
+  height: 64px;
+}
+
+.footer-language-menu {
+  @apply text-pernil-200 mx-auto;
+}
+</style>
---
+++ components/social-links/SocialLinks.vue
@@ -0,0 +1,22 @@
+<template>
+  <ul class="social-links" role="menu">
+    <li v-for="link in links" :key="link.id" role="menuitem">
+      <a
+        :href="link.url"
+        class="social-link"
+        :title="link.app"
+        rel="noopener"
+        target="_blank"
+      >
+        <arepa-icon :width="'24'" :height="'24'" :name="link.icon"></arepa-icon>
+      </a>
+    </li>
+  </ul>
+</template>
+
+<script>
+const SOCIAL_LINKS = [];
+export default {};
+</script>
+
+<style></style>
